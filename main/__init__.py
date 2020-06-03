537bef610caeaf56837225a134944a80fda2e59c
Yash Chaudhari <yash56244@gmail.com>
2020-06-03 13:45:18 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
 from flask_migrate import Migrate
 
 app = Flask(__name__)
