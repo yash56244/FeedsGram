41a4f4aeee770bdbda7c9924efbd25e198e5deaa
Yash Chaudhari <yash56244@gmail.com>
2020-06-05 16:13:04 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
@@ -5,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '3b81a7164a731c013a39bcdad657becb'
+app.config['SECRET_KEY'] = os.environ.get('SECRET')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
